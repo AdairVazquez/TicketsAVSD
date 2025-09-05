89a6c6351f42f1f6af1c578ce12d8f002d57949b
Adair Vazquez <adairvazquezcr@gmail.com>
2025-09-05 00:05:20 -0600

    Primer commit: proyecto TicketsAVSD

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+@section('title', 'Inicio')
+
+@section('content_header')
+    <h1>Bienvenido!</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido de nuevo</p>
+@stop
+
